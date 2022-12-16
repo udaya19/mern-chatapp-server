8f777d61bd75dc18bb4f801206d901de8968c85e
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-12-16 12:22:39 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 try {
@@ -16,6 +17,7 @@ try {
   console.log("failed");
 }
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/users", require("./routes/user"));
 const port = process.env.PORT;
 app.listen(port, () => {
