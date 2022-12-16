d8dbb27aea6a59d9b94c9715b889c97a0dafc704
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-12-16 10:22:21 +0530

    Login user functionality

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -24,6 +25,40 @@ router.post("/register", async (req, res) => {
       success: true,
       message: "Account created succesfully",
       newUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        error: "Incorrect email/password",
+      });
+    }
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      return res.status(400).json({
+        success: false,
+        error: "Incorrect email/password",
+      });
+    }
+    const token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user,
+      token,
     });
   } catch (error) {
     return res.status(500).json({
